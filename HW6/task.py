64e0e5fcd9d99f8182addeb2583753aeba6422f7
Alexandra2025 <turkova2025@gmail.com>
2024-02-19 16:44:00 +0800

    Add HW6.

---
+++ HW6/task.py
@@ -0,0 +1,23 @@
+# Написать программу на любом языке в любой парадигме для
+# бинарного поиска. На вход подаётся целочисленный массив и
+# число. На выходе - индекс элемента или -1, в случае если искомого
+# элемента нет в массиве.
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+arr = [5, 7, 9, 2, 4, 3]
+target = 4
+result = binary_search(arr, target)
+print(result)
